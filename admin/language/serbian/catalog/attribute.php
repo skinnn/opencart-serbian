f20645b46f42c86ab84213f89332f4d05409b7c6
Nikola <nikolahiphop@gmail.com>
2017-11-27 20:16:54 +0100

    Update attribute.php
    Nekoliko typo-a

--- admin/language/serbian/catalog/attribute.php
+++ admin/language/serbian/catalog/attribute.php
@@ -3,7 +3,7 @@
 $_['heading_title']          = 'Atributi';
 
 // Text
-$_['text_success']           = 'Uspešno je izmenjen atrinut!';
+$_['text_success']           = 'Uspešno je izmenjen atribut!';
 
 // Column
 $_['column_name']            = 'Ime atributa';
@@ -18,6 +18,6 @@ $_['entry_sort_order']      = 'Sortiranje:';
 
 // Error
 $_['error_permission']      = 'Nemate dozvolu da menjate atribute!';
-$_['error_name']            = 'Ime atributa mora sadržati minimalno  3 a maksimalno  64 karaktera!';
-$_['error_product']         = 'Ovaj atribut se ne može izbrisati jer je dodeljen  %s artiklima!';
+$_['error_name']            = 'Ime atributa mora sadržati minimalno 3 a maksimalno 64 karaktera!';
+$_['error_product']         = 'Ovaj atribut se ne može izbrisati jer je dodeljen %s artiklima!';
 ?>
